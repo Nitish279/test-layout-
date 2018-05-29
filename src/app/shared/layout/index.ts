fda6373dc69b984ce1d6350f58f945d496d648f2
Nitish279 <nitish4u029@gmail.com>
2018-05-29 22:09:48 +0530

    Layout index.js issue fixed

--- src/app/shared/layout/index.ts
+++ src/app/shared/layout/index.ts
@@ -3,4 +3,4 @@ export * from './header/header.component';
 export * from './sidebar/sidebar.component';
 export * from './subheader/subheader.component';
 export * from './leftnavbar/leftnavbar.component';
-export * from './layoutcontent/layoutcontent.component';
+export * from './leftnavbarcontent/leftnavbarcontent.component';
